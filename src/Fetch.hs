6f2ade704988e385fe32fafc91be5eed5e105e1c
cobaltinferno <nil>
2022-12-30 23:58:50 -0500

    Add fetch file

--- src/Fetch.hs
+++ src/Fetch.hs
@@ -1,6 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Fetch where
 
 import Control.Monad.Trans.Resource (runResourceT)
 import Data.Conduit.Combinators (sinkFile)
+import Data.Text (Text, splitOn)
 import Network.HTTP.Client.Conduit (parseRequest)
 import Network.HTTP.Simple (httpSink)
+import Text.StringConvert (toString)
+
+fetchFile :: Text -> IO ()
+fetchFile url =
+  parseRequest sUrl >>= \request ->
+    runResourceT $
+    httpSink request $ \_ -> sinkFile $ toString $ last $ splitOn "/" url
+  where
+    sUrl = toString url
